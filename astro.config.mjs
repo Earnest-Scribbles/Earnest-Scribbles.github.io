bd397ee7b7782888c9fc8dac5a4b0d618d213c39
Earnest-Scribbles <jack26blaze@gmail.com>
2025-10-25 23:20:56 +0530

    Add basic search functionality using pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import tailwind from '@astrojs/tailwind';
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,5 @@ export default defineConfig({
   },
   integrations: [tailwind({
     applyBaseStyles: false,
-  })]
+  }), pagefind()],
 });
